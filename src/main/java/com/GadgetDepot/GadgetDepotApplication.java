486237c6cda11447216781d4bd191aef5a46a1d4
parmar-chirayu <cyparmar@my.yorku.ca>
2024-01-17 23:15:52 -0500

    Project Setup

---
+++ src/main/java/com/GadgetDepot/GadgetDepotApplication.java
@@ -0,0 +1,13 @@
+package com.GadgetDepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetDepotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetDepotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GadgetDepot/GadgetDepotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GadgetDepot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetDepotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
